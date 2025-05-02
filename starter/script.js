15af6df062cfc88b70e9ea3df7169d384a495d14
Ehicodes <osaigbovoehinomen@gmail.com>
2025-05-02 22:32:37 +0100

    for of loop

--- starter/script.js
+++ starter/script.js
@@ -72,6 +72,17 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
+
+//FOR LOOP
+
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu);
+for (const item of menu) console.log(item);
+
+for (const [i, el] of menu.entries()) {
+  console.log(`${i + 1}: ${el}`);
+}
+
 /*
 /////////the nullish coalescing operator
  restaurant.numGuests = 0;
