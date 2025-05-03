c44b259ab3cc10f8667d36b9e8328bf409abfa48
Ehicodes <osaigbovoehinomen@gmail.com>
2025-05-03 22:03:50 +0100

    computing property names

--- starter/script.js
+++ starter/script.js
@@ -1,28 +1,23 @@
 'use strict';
+//enhanced object literal. computing the property names
+const weekDays = ['mon', 'tues', 'wed', 'thurs', 'fri', 'sat', 'sun'];
 
-// Data needed for a later exercise
-// const flights =
-//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+const openingHours = {
+  [weekDays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekDays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [`day-${2 + 4}`]: {
+    open: 0, //Open 24 hours
+    close: 24,
+  },
+};
 
-// const italianFoods = new Set([
-//   'pasta',
-//   'gnocchi',
-//   'tomatoes',
-//   'olive oil',
-//   'garlic',
-//   'basil',
-// ]);
-
-// const mexicanFoods = new Set([
-//   'tortillas',
-//   'beans',
-//   'rice',
-//   'tomatoes',
-//   'avocado',
-//   'garlic',
-// ]);
-
-// Data needed for first part of the section
+console.log(openingHours);
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -30,44 +25,27 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, //Open 24 hours
-      close: 24,
-    },
-  },
+  //Enhanced ES6 Object Literal
+  openingHours,
 
-  order: function (starterIndex, mainIndex) {
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
   //Practical Application  of Destructuring
-  orderDelivery: function ({
-    starterIndex = 1,
-    mainIndex = 0,
-    time = '20:00',
-    address,
-  }) {
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
     console.log(
       `Order Received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
     );
   },
 
-  orderPasta: function (ing1, ing2, ing3) {
+  orderPasta(ing1, ing2, ing3) {
     console.log(
       `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
     );
   },
   //rest parameter
-  orderPizza: function (mainIngredient, ...otherIngredients) {
+  orderPizza(mainIngredient, ...otherIngredients) {
     console.log(mainIngredient);
     console.log(otherIngredients);
   },
@@ -75,13 +53,13 @@ const restaurant = {
 
 //FOR LOOP
 
-const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
-console.log(menu);
-for (const item of menu) console.log(item);
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+// console.log(menu);
+// for (const item of menu) console.log(item);
 
-for (const [i, el] of menu.entries()) {
-  console.log(`${i + 1}: ${el}`);
-}
+// for (const [i, el] of menu.entries()) {
+//   console.log(`${i + 1}: ${el}`);
+// }
 
 /*
 /////////the nullish coalescing operator
