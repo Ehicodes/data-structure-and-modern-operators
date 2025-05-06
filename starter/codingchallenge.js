889af33d117a25cb00516631f2d328b01cc806c2
Ehicodes <osaigbovoehinomen@gmail.com>
2025-05-06 06:51:30 +0100

    coding challenge 1. task 1

--- starter/codingchallenge.js
+++ starter/codingchallenge.js
@@ -73,46 +73,78 @@ const game = {
 // const [players1] = game.players;
 // console.log(players1);
 
-const [players1, players2] = game.players;
-const secondTeam = [...players2];
-console.log(players1, players2);
+// const [players1, players2] = game.players;
+// const secondTeam = [...players2];
+// console.log(players1, players2);
 // console.log(players1, secondTeam);
 
 //2
-const [gk, ...fieldPlayers] = players1;
-const [gk2, ...fieldPlayers2] = secondTeam;
-console.log(gk, fieldPlayers);
-console.log(gk2, fieldPlayers2);
+// const [gk, ...fieldPlayers] = players1;
+// const [gk2, ...fieldPlayers2] = secondTeam;
+// console.log(gk, fieldPlayers);
+// console.log(gk2, fieldPlayers2);
 
 //3
-const allPlayers = [...players1, ...players2];
-console.log(allPlayers);
+// const allPlayers = [...players1, ...players2];
+// console.log(allPlayers);
 
 //4
-const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
-console.log(players1Final);
+// const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+// console.log(players1Final);
 
 //5
-//TUTOR CODE CORRECTIONS
 //nested destructuring
-const {
-  odds: { team1, x: draw, team2 },
-} = game;
-console.log(team1, draw, team2);
+// const {
+//   odds: { team1, x: draw, team2 },
+// } = game;
+// console.log(team1, draw, team2);
 
 //6
-//TUTOR CODE CORRECTIONS
-const printGoals = function (...numbersOfPlayers) {
-  console.log(`${numbersOfPlayers.length} goals were scored`);
-};
-printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
-printGoals('Coman', 'Pavard', 'Goretzka');
-printGoals(...game.scored);
+// const printGoals = function (...numbersOfPlayers) {
+//   console.log(`${numbersOfPlayers.length} goals were scored`);
+// };
+// printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+// printGoals('Coman', 'Pavard', 'Goretzka');
+// printGoals(...game.scored);
 
 //7
 //TUTOR CODE
-team1 < team2 && console.log('Team 1 is likely to win');
-team2 < team1 && console.log('Team 2 is likely to win');
+// team1 < team2 && console.log('Team 1 is likely to win');
+// team2 < team1 && console.log('Team 2 is likely to win');
 // const moreLikelyToWin =
 //   (team1 < team2 && 'Team 1') || (team2 < team1 && 'Team 2');
 // console.log(`${moreLikelyToWin} is more likely to win`);
+
+/*
+
+Coding Challenge #2
+Let's continue with our football betting app! Keep using the 'game' variable from
+before.
+Your tasks:
+1. Loop over the game.scored array and print each player name to the console,
+along with the goal number (Example: "Goal 1: Lewandowski")
+2. Use a loop to calculate the average odd and log it to the console (We already
+studied how to calculate averages, you can go check if you don't remember)
+3. Print the 3 odds to the console, but in a nice formatted way, exactly like this:
+Odd of victory Bayern Munich: 1.33
+Odd of draw: 3.25
+Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them
+(except for "draw"). Hint: Note how the odds and the game objects have the
+same property names 😉
+4. Bonus: Create an object called 'scorers' which contains the names of the
+players who scored as properties, and the number of goals as the value. In this
+game, it will look like this:
+{
+Gnarby: 1,
+Hummels: 1,
+Lewandowski: 2
+}
+*/
+
+//1
+for (const [i, el] of game.scored.entries()) {
+  console.log(`Goal ${i + 1}: ${el}`);
+}
+
+//2
