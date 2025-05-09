b5186725476979bc884f086fd46ad006bc2a073e
Ehicodes <osaigbovoehinomen@gmail.com>
2025-05-09 06:08:50 +0100

    Map iteration

--- starter/script.js
+++ starter/script.js
@@ -417,3 +417,40 @@ console.log(rest);
 console.log(rest.size);
 
 console.log(rest.get(arr));
+
+//Map iteration
+const question = new Map([
+  ['question', 'What is the best programming languagen in the world'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct'],
+  [false, 'Try Again'],
+]);
+
+console.log(question);
+
+//convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+//Quiz app
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}:${value}`);
+}
+
+// const answer = Number(prompt('Your answer'));
+const answer = 3;
+console.log(answer);
+
+console.log(question.get(question.get('correct') === answer));
+
+//convert map to array
+console.log([...question]);
+
+console.log([...question.keys()]);
+console.log(question.values());
+console.log(question.entries());
