a5658f97557885fefb27842b4950544fa12886c9
Ehicodes <osaigbovoehinomen@gmail.com>
2025-05-11 05:26:30 +0100

    working with strings. part 1

--- starter/script.js
+++ starter/script.js
@@ -1,54 +1,54 @@
 'use strict';
 //enhanced object literal. computing the property names
-const weekDays = ['mon', 'tues', 'wed', 'thurs', 'fri', 'sat', 'sun'];
+// const weekDays = ['mon', 'tues', 'wed', 'thurs', 'fri', 'sat', 'sun'];
 
-const openingHours = {
-  [weekDays[3]]: {
-    open: 12,
-    close: 22,
-  },
-  [weekDays[4]]: {
-    open: 11,
-    close: 23,
-  },
-  [weekDays[5]]: {
-    open: 0, //Open 24 hours
-    close: 24,
-  },
-};
+// const openingHours = {
+//   [weekDays[3]]: {
+//     open: 12,
+//     close: 22,
+//   },
+//   [weekDays[4]]: {
+//     open: 11,
+//     close: 23,
+//   },
+//   [weekDays[5]]: {
+//     open: 0, //Open 24 hours
+//     close: 24,
+//   },
+// };
 
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+// const restaurant = {
+//   name: 'Classico Italiano',
+//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
+//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  //Enhanced ES6 Object Literal
-  openingHours,
+//Enhanced ES6 Object Literal
+// openingHours,
 
-  order(starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  },
+// order(starterIndex, mainIndex) {
+//   return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+// },
 
-  //Practical Application  of Destructuring
-  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
-    console.log(
-      `Order Received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
-    );
-  },
+//Practical Application  of Destructuring
+// orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+//   console.log(
+//     `Order Received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+//   );
+// },
 
-  orderPasta(ing1, ing2, ing3) {
-    console.log(
-      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
-    );
-  },
-  //rest parameter
-  orderPizza(mainIngredient, ...otherIngredients) {
-    console.log(mainIngredient);
-    console.log(otherIngredients);
-  },
-};
+// orderPasta(ing1, ing2, ing3) {
+//   console.log(
+//     `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
+//   );
+// },
+//rest parameter
+//   orderPizza(mainIngredient, ...otherIngredients) {
+//     console.log(mainIngredient);
+//     console.log(otherIngredients);
+//   },
+// };
 //looping over object names in an indirect way
 //PROPERTY NAMES
 // const properties = Object.keys(openingHours);
@@ -352,105 +352,137 @@ const [p = 1, q = 1, r = 1] = [8];
 console.log(p, q, r);
 */
 
-//SETS
-const ordersSet = new Set([
-  'Pasta',
-  'Pizza',
-  'Pizza',
-  'Risotto',
-  'Pasta',
-  'Pizza',
-]);
-console.log(ordersSet);
-console.log(new Set('Ehi'));
-console.log(ordersSet.size); //checking the size.
-console.log(ordersSet.has('Bread'));
-console.log(ordersSet.has('Pizza')); //checking if an element is in the set
-ordersSet.add('Garlic Bread'); //adding a new element to the set
-console.log(ordersSet);
-ordersSet.delete('Risotto'); //deleting an element
-console.log(ordersSet);
+// //SETS
+// const ordersSet = new Set([
+//   'Pasta',
+//   'Pizza',
+//   'Pizza',
+//   'Risotto',
+//   'Pasta',
+//   'Pizza',
+// ]);
+// console.log(ordersSet);
+// console.log(new Set('Ehi'));
+// console.log(ordersSet.size); //checking the size.
+// console.log(ordersSet.has('Bread'));
+// console.log(ordersSet.has('Pizza')); //checking if an element is in the set
+// ordersSet.add('Garlic Bread'); //adding a new element to the set
+// console.log(ordersSet);
+// ordersSet.delete('Risotto'); //deleting an element
+// console.log(ordersSet);
 
-for (const order of ordersSet) {
-  console.log(order);
-}
+// for (const order of ordersSet) {
+//   console.log(order);
+// }
 
-//Use case for sets. Example
-const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+// //Use case for sets. Example
+// const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
 
-const staffUnique = [...new Set(staff)];
-console.log(staffUnique);
+// const staffUnique = [...new Set(staff)];
+// console.log(staffUnique);
 
-console.log(
-  new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size
-);
-//
-console.log(new Set('OsaigbovoEhinomen').size);
+// console.log(
+//   new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size
+// );
+// //
+// console.log(new Set('OsaigbovoEhinomen').size);
 
-//MAPS
-const rest = new Map();
+// //MAPS
+// const rest = new Map();
 
-rest.set('name', 'Classico Italiano');
-rest.set(1, 'Firenze, Italy');
-console.log(rest.set(2, 'Lisbon, Portugal'));
-rest
-  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
-  .set('open', 11)
-  .set('close', 23)
-  .set(true, 'we are open')
-  .set(false, 'we are closed');
+// rest.set('name', 'Classico Italiano');
+// rest.set(1, 'Firenze, Italy');
+// console.log(rest.set(2, 'Lisbon, Portugal'));
+// rest
+//   .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+//   .set('open', 11)
+//   .set('close', 23)
+//   .set(true, 'we are open')
+//   .set(false, 'we are closed');
 
-console.log(rest.get('name'));
-// console.log(rest.get(true));
-console.log(rest.get(1));
+// console.log(rest.get('name'));
+// // console.log(rest.get(true));
+// console.log(rest.get(1));
 
-const time = 8;
-console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+// const time = 8;
+// console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
 
-console.log(rest.has('categories'));
-rest.delete(2);
-// rest.clear()
-const arr = [1, 2];
-rest.set(arr, 'Test');
-rest.set(document.querySelector('h1', 'Heading'));
-console.log(rest);
-console.log(rest.size);
+// console.log(rest.has('categories'));
+// rest.delete(2);
+// // rest.clear()
+// const arr = [1, 2];
+// rest.set(arr, 'Test');
+// rest.set(document.querySelector('h1', 'Heading'));
+// console.log(rest);
+// console.log(rest.size);
 
-console.log(rest.get(arr));
+// console.log(rest.get(arr));
 
-//Map iteration
-const question = new Map([
-  ['question', 'What is the best programming languagen in the world'],
-  [1, 'C'],
-  [2, 'Java'],
-  [3, 'JavaScript'],
-  ['correct', 3],
-  [true, 'Correct'],
-  [false, 'Try Again'],
-]);
+// //Map iteration
+// const question = new Map([
+//   ['question', 'What is the best programming languagen in the world'],
+//   [1, 'C'],
+//   [1, 'C'],
+//   [2, 'Java'],
+//   [3, 'JavaScript'],
+//   ['correct', 3],
+//   [true, 'Correct'],
+//   [false, 'Try Again'],
+// ]);
 
-console.log(question);
+// console.log(question);
 
-//convert object to map
-console.log(Object.entries(openingHours));
-const hoursMap = new Map(Object.entries(openingHours));
-console.log(hoursMap);
+// //convert object to map
+// console.log(Object.entries(openingHours));
+// const hoursMap = new Map(Object.entries(openingHours));
+// console.log(hoursMap);
 
-//Quiz app
-console.log(question.get('question'));
-for (const [key, value] of question) {
-  if (typeof key === 'number') console.log(`Answer ${key}:${value}`);
-}
+// //Quiz app
+// console.log(question.get('question'));
+// for (const [key, value] of question) {
+//   if (typeof key === 'number') console.log(`Answer ${key}:${value}`);
+// }
 
-// const answer = Number(prompt('Your answer'));
-const answer = 3;
-console.log(answer);
+// // const answer = Number(prompt('Your answer'));
+// const answer = 3;
+// console.log(answer);
 
-console.log(question.get(question.get('correct') === answer));
+// console.log(question.get(question.get('correct') === answer));
 
-//convert map to array
-console.log([...question]);
+// //convert map to array
+// console.log([...question]);
 
-console.log([...question.keys()]);
-console.log(question.values());
-console.log(question.entries());
+// console.log([...question.keys()]);
+// console.log(question.values());
+// console.log(question.entries());
+
+/*//////////////////////WORKING WITH STRINGS////////////////////////////////// */
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
+console.log(plane);
+console.log(plane[0]);
+console.log('B737'[0]);
+console.log(airline.length);
+console.log('B737'.length);
+console.log(airline.indexOf('r'));
+console.log(airline.indexOf('Portugal'));
+
+//A GOOD USE CASE
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7));
+
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+
+console.log(airline.slice(3));
+console.log(airline.slice(1, -1));
+const checkMiddleSeat = function (seat) {
+  //B and E are middle seats
+  const s = seat.slice(-1);
+  if (s === 'B' || s === 'E') console.log('You got the middle seat');
+  else console.log('You got lucky');
+};
+
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
