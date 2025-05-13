86e3807a59280970750f1da23a4a0f71bad4efa9
Ehicodes <osaigbovoehinomen@gmail.com>
2025-05-13 02:25:42 +0100

    coding challenge 4

--- starter/codingchallenge.js
+++ starter/codingchallenge.js
@@ -206,33 +206,62 @@ const gameEvents = new Map([
 ]);
 
 //1
-const events = [...new Set(gameEvents.values())];
-console.log(events);
+// const events = [...new Set(gameEvents.values())];
+// console.log(events);
 
 //2
-gameEvents.delete(64);
-console.log(gameEvents);
+// gameEvents.delete(64);
+// console.log(gameEvents);
 
 //3
-const average = 90 / gameEvents.size;
-console.log(`An event happened, on average, every ${average} minutes`);
+// const average = 90 / gameEvents.size;
+// console.log(`An event happened, on average, every ${average} minutes`);
 
 //TUTOR CODE. BONUS
-const extraTime = [...gameEvents.keys()].pop();
-console.log(extraTime);
-console.log(
-  `An event happened, on average, every ${
-    extraTime / gameEvents.size
-  }   minutes`
-);
+// const extraTime = [...gameEvents.keys()].pop();
+// console.log(extraTime);
+// console.log(
+//   `An event happened, on average, every ${
+//     extraTime / gameEvents.size
+//   }   minutes`
+// );
 
-4;
-for (const [time, event] of gameEvents) {
-  const str = time < 45 ? '[FIRST HALF]' : '[SECOND HALF]';
-  console.log(`${str} ${time}: ${event}`);
-}
+// 4;
+// for (const [time, event] of gameEvents) {
+//   const str = time < 45 ? '[FIRST HALF]' : '[SECOND HALF]';
+//   console.log(`${str} ${time}: ${event}`);
+// }
 //TUTOR CODE
-for (const [min, event] of gameEvents) {
-  const half = min < 45 ? 'FIRST' : 'SECOND';
-  console.log(`[${half} HALF] ${min} ${event}`);
-}
+// for (const [min, event] of gameEvents) {
+//   const half = min < 45 ? 'FIRST' : 'SECOND';
+//   console.log(`[${half} HALF] ${min} ${event}`);
+// }
+
+/*///////////////////////////////
+Coding Challenge #4
+Write a program that receives a list of variable names written in underscore_case
+and convert them to camelCase.
+The input will come from a textarea inserted into the DOM (see code below to
+insert the elements), and conversion will happen when the button is pressed.
+Test data (pasted to textarea, including spaces):
+underscore_case
+first_name
+Some_Variable
+calculate_AGE
+delayed_departure
+Should produce this output (5 separate console.log outputs):
+underscoreCase ✅
+firstName ✅✅
+someVariable ✅✅✅
+calculateAge ✅✅✅✅
+delayedDeparture ✅✅✅✅✅
+
+Hints:
+§ Remember which character defines a new line in the textarea 😉
+§ The solution only needs to work for a variable made out of 2 words, like a_b
+§ Start without worrying about the ✅. Tackle that only after you have the variable
+name conversion working 😉
+§ This challenge is difficult on purpose, so start watching the solution in case
+you're stuck. Then pause and continue!
+Afterwards, test with your own test data!
+*/
