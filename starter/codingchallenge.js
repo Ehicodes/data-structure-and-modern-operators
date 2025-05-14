0059ab52766bedfc466941c7be3590d419a9b4ea
Ehicodes <osaigbovoehinomen@gmail.com>
2025-05-14 03:33:31 +0100

    coding challenge 4. task 2

--- starter/codingchallenge.js
+++ starter/codingchallenge.js
@@ -272,17 +272,14 @@ document.body.append(document.createElement('button'));
 
 document.querySelector('button').addEventListener('click', function () {
   const text = document.querySelector('textarea').value;
-
   const rows = text.split('\n');
-
-  for (const row of rows) {
-    const [first, second] = row.trim().toLowerCase().split('_');
-
-    const ouput = `${first}${second.replace(
+  for (const [i, row] of rows.entries()) {
+    const [first, second] = row.toLowerCase().trim().split('_');
+    const output = `${first}${second.replace(
       second[0],
       second[0].toUpperCase()
     )}`;
-    console.log(ouput);
+    console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
   }
 });
 
